da6602e3b98499acb46f913569642c470e255c24
Jedrzej Siemienski <jedrzej.siemienski@infullmobile.com>
2016-12-09 16:28:51 +0100

    added spring jars to pom and basic spring configuration, added basci test controller

---
+++ src/main/java/com/jedrek/urticaRecruitmentTask/controller/CustomersController.java
@@ -0,0 +1,18 @@
+package com.jedrek.urticaRecruitmentTask.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+//@RequestMapping("/")
+public class CustomersController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+	
+}
