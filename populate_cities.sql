b72f19b245644068a7e57eba0ee2f8cb2c996dc0
Jedrzej Siemienski <jedrzej.siemienski@infullmobile.com>
2016-12-10 17:19:06 +0100

    added sql with cities

--- populate_cities.sql
+++ populate_cities.sql
@@ -1,4 +1,4 @@
-INSERT INTO City ( name ) VALUES ( "Wrocław" );
+INSERT INTO City ( name ) VALUES ( "Wroclaw" );
 INSERT INTO City ( name ) VALUES ( "Warszawa" );
 INSERT INTO City ( name ) VALUES ( "Londyn" );
-INSERT INTO City ( name ) VALUES ( "Lądek zdrój" );
+INSERT INTO City ( name ) VALUES ( "Ladek zdroj" );
